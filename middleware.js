11c6026ed96d6741d44a80cfce0972ba38adb58d
Shivanand-0 <ishivanandgupta@gmail.com>
2025-06-10 21:53:28 +0530

    feat: Listing Authorization

--- middleware.js
+++ middleware.js
@@ -1,3 +1,36 @@
+const Listing=require("./models/listing")
+const {validListingSchema, validReviewSchema}=require("./Schema")
+const ExpressError=require("./utils/ExpressError.js")
+
+
+
+
+// database listing Schema validation middelware (Server side validation)
+module.exports.validateListing=(req,resp,next)=>{
+    let {error}=validListingSchema.validate(req.body.listing);
+    if(error){
+        let errorMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errorMsg);
+    }
+    else{
+        next();
+    }
+}
+
+// database review Schema validation middelware (Server side validation)
+module.exports.validateReview=(req,resp,next)=>{
+    let {error}=validReviewSchema.validate(req.body.review);
+    if(error){
+        let errorMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errorMsg);
+    }
+    else{
+        next();
+    }
+
+}
+
+// authentication middleware
 module.exports.isLoggedIn=(req,resp,next)=>{
     if(! req.isAuthenticated()){
         req.session.redirectPath=req.originalUrl
@@ -13,3 +46,14 @@ module.exports.redirectPath=(req,resp,next)=>{
     }
     next()
 }
+// middleware for authorizatin
+module.exports.isAutherized=async(req,resp,next)=>{
+    let id=req.params.id;
+    let listinginfo=await Listing.findById(id);
+    if(! listinginfo.owner.equals(resp.locals.currUser._id)){
+        req.flash("error","You don't have permission!!!")
+        return resp.redirect(`/listings/${id}/details`)
+    }
+    next()
+}
+
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -3,24 +3,11 @@ const express=require("express");
 const router=express.Router({mergeParams:true});
 const Listing=require("../models/listing")
 const Review=require("../models/review.js")
-const {validReviewSchema}=require("../Schema.js")
 const wrapAsync=require("../utils/wrapAsync.js")
-const ExpressError=require("../utils/ExpressError.js")
-const {isLoggedIn}=require("../middleware.js");
+const {isLoggedIn,validateReview}=require("../middleware.js");
 
 
-// database Schema validation middelware (Server side validation)
-const validateReview=(req,resp,next)=>{
-    let {error}=validReviewSchema.validate(req.body.review);
-    if(error){
-        let errorMsg=error.details.map((el)=>el.message).join(",");
-        throw new ExpressError(400,errorMsg);
-    }
-    else{
-        next();
-    }
 
-}
 
 // 
 //all reviews routes
